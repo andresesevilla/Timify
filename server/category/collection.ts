b8f4af87c2c47418cfddf59182017b97765fe914
Andres Sevilla <andresesevilla@gmail.com>
2022-12-01 16:04:22 -0500

    Added category deletion sync

--- server/category/collection.ts
+++ server/category/collection.ts
@@ -2,6 +2,8 @@ import type { HydratedDocument, Types } from 'mongoose';
 import type { Category, PopulatedCategory } from './model';
 import CategoryModel from './model';
 import UserCollection from '../user/collection';
+import GoalCollection from '../goal/collection';
+import EntryCollection from '../entry/collection';
 
 class CategoryCollection {
   static async findAllByUserId(userId: Types.ObjectId | string): Promise<Array<HydratedDocument<Category>>> {
@@ -31,8 +33,17 @@ class CategoryCollection {
 
   static async deleteOne(userId: Types.ObjectId | string, name: string): Promise<boolean> {
     const user = await UserCollection.findOneByUserId(userId);
-    const category = await CategoryModel.deleteOne({ userId: user._id, name: name });
-    return category !== null;
+    const category = await CategoryCollection.findByNameAndUserId(userId, name);
+
+    // Delete corresponding goal if it exists
+    const goal = await GoalCollection.findOneByCategoryId(category.id);
+    await GoalCollection.deleteOne(goal.id);
+
+    // Delete all time entries with this category
+    await EntryCollection.deleteAllInCategory(category.id);
+
+    const deletedCategory = await CategoryModel.deleteOne({ userId: user._id, name: name });
+    return deletedCategory !== null;
   }
 
   static async renameOne(userId: Types.ObjectId | string, oldName: string, newName: string): Promise<HydratedDocument<Category>> {
