53b1f726a1b7958cb673b97ea27d313850512467
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-12-03 02:15:06 -0500

    working calendar!

--- client/components/Calendar/DailyCalendar.vue
+++ client/components/Calendar/DailyCalendar.vue
@@ -2,14 +2,14 @@
   <section class="calendar-container">
     <FullCalendar ref="fullCalendar" :options="calendarOptions">
       <template v-slot:eventContent="arg">
-        <b>{{ arg.timeText }}</b>
-        <i>{{ arg.event.title }}</i>
+        <div class="event-content">
+          <b>{{ arg.timeText }}</b>
+          <i>{{ arg.event.title }}</i>
+        </div>
       </template>
     </FullCalendar>
     <transition name="fade">
       <nav class="card event-control" v-if="eventSelected">
-
-
         <div class="card-wrapper" v-if="!editingSelected">
           <header class="card-header">
             <p class="card-header-title">{{ eventSelected.title }}</p>
@@ -25,22 +25,35 @@
           </footer>
         </div>
 
-
         <div class="card-wrapper" v-else>
           <header class="card-header">
             <p class="card-header-title">
-              <CategoryAutocomplete 
-                :value="eventSelected.title" :categories="categories" :loading="categoriesLoading"
-                @select="updateEventSelected({'title': $event})"
+              <CategoryAutocomplete
+                :value="eventSelected.title"
+                :categories="categories"
+                :loading="categoriesLoading"
+                @select="updateEventSelected({ title: $event })"
+                ref="categoryAutocomplete"
               />
             </p>
           </header>
           <div class="card-content">
             <div class="content">
               <span>
-                <b-timepicker v-model="eventDraft.start" @input="eventSelected.setStart(eventDraft.start)" inline :increment-minutes="5"></b-timepicker> 
+                <b-timepicker
+                  v-model="eventDraft.start"
+                  @input="eventSelected.setStart(eventDraft.start)"
+                  inline
+                  :increment-minutes="5"
+                ></b-timepicker>
                 to
-                <b-timepicker v-model="eventDraft.end" @input="eventSelected.setEnd(eventDraft.end)" inline :increment-minutes="5"></b-timepicker></span>
+                <b-timepicker
+                  v-model="eventDraft.end"
+                  @input="eventSelected.setEnd(eventDraft.end)"
+                  inline
+                  :increment-minutes="5"
+                ></b-timepicker
+              ></span>
             </div>
           </div>
           <footer class="card-footer">
@@ -63,7 +76,8 @@ import CategoryAutocomplete from "@/components/Category/CategoryAutocomplete.vue
 export default {
   name: "DailyCalendar",
   components: {
-    FullCalendar, CategoryAutocomplete
+    FullCalendar,
+    CategoryAutocomplete,
   },
   data() {
     return {
@@ -72,27 +86,27 @@ export default {
         allDaySlot: false,
         nowIndicator: true,
         initialView: "timeGridDay",
+
         editable: true,
         selectable: true,
-        select: this.handleDateSelect,
+        eventResizableFromStart: true,
         selectMirror: true,
+
+        select: this.handleDateSelect,
         eventClick: this.handleEventClick,
         eventsSet: this.handleEvents,
+
+        eventResizeStart: this.handleEventResizeStart,
+        eventResize: this.handleEventResize,
+        eventDragStart: this.handleEventDragStart,
+        eventDrop: this.handleEventDrag,
+
         eventShortHeight: 15,
         slotDuration: "00:15:00",
         scrollTime: new Date().getHours() - 1 + ":00:00",
-        events: [
-          {
-            title: "haha 1",
-            start: "2022-12-02T16:00:00",
-            end: "2022-12-02T16:18:00",
-          },
-          {
-            title: "haha 2",
-            start: "2022-12-02T21:00:00",
-            end: "2022-12-02T22:53:13",
-          },
-        ],
+
+        events: [],
+        eventOverlap: false,
       },
       eventSelected: null,
       editingSelected: false,
@@ -113,14 +127,29 @@ export default {
   },
   mounted() {
     // get categories
-    fetch('/api/categories')
-      .then(response => response.json())
-      .then(categories => {
-        this.categories = categories.map(category => category.name);
+    fetch("/api/categories")
+      .then((response) => response.json())
+      .then((categories) => {
+        this.categories = categories.map((category) => category.name);
         this.categoriesLoading = false;
       });
+    this.fetchEvents();
   },
   methods: {
+    fetchEvents() {
+      fetch("/api/entries")
+        .then((response) => response.json())
+        .then((entries) => {
+          this.calendarOptions.events = entries.map((entry) => {
+            return {
+              id: entry._id,
+              title: entry.category,
+              start: entry.start,
+              end: entry.end,
+            };
+          });
+        });
+    },
     hmTime(date) {
       return date.toLocaleTimeString("en-US", {
         hour: "numeric",
@@ -129,25 +158,42 @@ export default {
     },
     handleDateSelect(selectionInfo) {
       const calendarApi = this.$refs.fullCalendar.getApi();
-
       this.eventSelected = calendarApi.addEvent({
-        id: this.currentEvents.length + 1,
+        id: "",
         title: "",
         start: selectionInfo.startStr,
         end: selectionInfo.endStr,
         allDay: selectionInfo.allDay,
       });
+      this.eventSelected.draft = true;
       this.editSelected();
-
       calendarApi.unselect();
     },
     handleEventClick(clickInfo) {
       if (this.eventSelected && this.eventSelected.id === clickInfo.event.id) {
         this.eventSelected = null;
+        this.editingSelected = false;
       } else {
         this.eventSelected = clickInfo.event;
       }
     },
+    handleEventResize(eventResizeInfo) {
+      this.tryUpdateEvent(eventResizeInfo.event);
+    },
+    handleEventDrag(eventDragInfo) {
+      this.tryUpdateEvent(eventDragInfo.event);
+    },
+    tryUpdateEvent(event) {
+      if (!this.isValidEvent(event)) {
+        return;
+      }
+      this.eventSelected = event;
+      this.saveSelected();
+    },
+    isValidEvent(event) {
+      return true;
+    },
+
     handleEvents(events) {
       this.currentEvents = events;
     },
@@ -159,25 +205,75 @@ export default {
       };
     },
     deleteSelected() {
-      if (this.eventSelected) {
-        this.eventSelected.remove();
-        this.eventSelected = null;
-      }
+      fetch(`/api/entries/${this.eventSelected.id}`, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.error) {
+            this.$buefy.toast.open({
+              message: response.error,
+              type: "is-danger",
+            });
+            this.fetchEvents();
+            return;
+          }
+          this.eventSelected.remove();
+          this.eventSelected = null;
+        });
     },
     saveSelected() {
-      this.eventSelected = null;
-      this.editingSelected = null;
+      const url = this.eventSelected.draft
+        ? "/api/entries"
+        : `/api/entries/${this.eventSelected.id}`;
+      fetch(url, {
+        method: this.eventSelected.draft ? "POST" : "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          category: this.eventSelected.title,
+          start: this.eventSelected.start,
+          end: this.eventSelected.end,
+        }),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.error) {
+            this.$buefy.toast.open({
+              message: response.error,
+              type: "is-danger",
+            });
+            this.fetchEvents();
+            return;
+          }
+          this.eventSelected.setProp("id", response.entry._id);
+          this.eventSelected = null;
+          this.editingSelected = false;
+        });
     },
     cancelSelected() {
+      if (this.eventSelected.draft) {
+        this.eventSelected.remove();
+      }
       this.eventSelected = null;
+      this.editingSelected = false;
     },
     updateEventSelected(update) {
-      console.log(update);
       if (update.title) {
-        this.eventSelected.setProp('title', update.title);
+        this.eventSelected.setProp("title", update.title);
       }
     },
   },
+  watch: {
+    eventSelected(newVal) {
+      if (newVal && this.editingSelected) {
+        this.$nextTick(() => {
+          this.$refs.categoryAutocomplete.focus();
+        });
+      }
+    }
+  }
 };
 </script>
 
@@ -204,7 +300,13 @@ export default {
 .event-control {
   z-index: 1000;
   position: absolute;
-  top: 50%;
+  top: 6em;
   right: 1em;
+}
+
+.event-content {
+  display: flex;
+  gap: 0.5em;
+  font-size: 1.2em;
 }
 </style>
